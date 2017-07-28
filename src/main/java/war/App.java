866951ca36a9bdba5653ece9a552a4118cc178d0
aspoddar <poddar.suresh@gmail.com>
2017-07-28 03:01:20 -0700

    ascSDV

--- src/main/java/war/App.java
+++ src/main/java/war/App.java
@@ -1,13 +1,11 @@
-package war;
+public class HelloWorld
 
-/**
- * Hello world!
- *
- */
-public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+
 }
